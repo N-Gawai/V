c276d5e7ca37c00550d9fe747e5e69bcf7b6ccb2
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-10-31 23:01:31 +0530

    input code adde

--- src/components/Ui/Input/Input.jsx
+++ src/components/Ui/Input/Input.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import classes from "./Input.module.css";
 
 const Input = () => {
-  return <div>Input</div>;
+  return (
+    <div>
+      <input
+        type="text"
+        name="text"
+        className={classes.input}
+        placeholder="Type here!"
+      ></input>
+    </div>
+  );
 };
 
 export default Input;
